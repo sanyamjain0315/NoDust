4e8d3005d1fff1f18c6f8ab9c3fdccdea0185020
sanyamjain0315 <sanyamj@bu.edu>
2026-05-13 21:07:04 -0400

    feat(pyspark): Pyspark anomaly detection outline
    - Created a basic framework to be able to detect anomalies with pyspark.
    - Current strategy is just averaging over a window and checking with a hard threshold
    - Debugged and settled on the pyspark and scala (spark-sql-kafka) version needed.
    - System also writes to alerts topic in kafka

--- system/dataflow.py
+++ system/dataflow.py
@@ -1,11 +1,67 @@
 import os
-from kafka import KafkaConsumer
 
-SENSOR_TOPIC = os.environ.get('SENSOR_TOPIC', "sensor")
-KAFKA_BOOTSTRAP_SERVERS = os.environ.get('KAFKA_BOOTSTRAP_SERVERS', 
-                                         "localhost:9092")
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, from_json, window
+from pyspark.sql.types import DoubleType, StringType, StructField, StructType
 
-consumer = KafkaConsumer(SENSOR_TOPIC, bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS)
+# Load config from Docker environment variables
+BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "kafka:29092")
+INPUT_TOPIC = os.getenv("SENSOR_TOPIC", "site-sensor-raw")
+OUTPUT_TOPIC = os.getenv("ALERTS_TOPIC", "alerts")
 
-for msg in consumer:
-    print (msg)
+spark = SparkSession.builder.appName("SensorAnomalyDetection").getOrCreate()
+
+schema = StructType(
+    [
+        StructField("sensor_id", StringType(), True),
+        StructField("site_id", StringType(), True),
+        StructField("timestamp", StringType(), True),
+        StructField("pollutant_type", StringType(), True),
+        StructField("concentration", DoubleType(), True),
+        StructField("unit", StringType(), True),
+    ]
+)
+
+
+df = (
+    spark.readStream.format("kafka")
+    .option("kafka.bootstrap.servers", BOOTSTRAP_SERVERS)
+    .option("subscribe", INPUT_TOPIC)
+    .option("startingOffsets", "earliest")
+    .load()
+)
+
+parsed = (
+    df.selectExpr("CAST(value AS STRING)")
+    .select(from_json(col("value"), schema).alias("data"))
+    .select("data.*")
+    .withColumn("event_time", col("timestamp").cast("timestamp"))
+)
+
+# 2. Stateful Aggregation (Average)
+windowed_aggs = (
+    parsed.withWatermark("event_time", "2 minutes")
+    .groupBy(
+        window(col("event_time"), "1 minute"), col("pollutant_type"), col("site_id")
+    )
+    .avg("concentration")  # Changed from max to avg
+    .withColumnRenamed("avg(concentration)", "avg_concentration")
+)
+
+# 3. Filter the Averages
+anomalies = windowed_aggs.filter(col("avg_concentration") > 50)
+
+# 4. Write Alerts to Kafka
+query = (
+    anomalies.selectExpr(
+        "CAST(site_id AS STRING) AS key", "to_json(struct(*)) AS value"
+    )
+    .writeStream.format("kafka")
+    .option("kafka.bootstrap.servers", BOOTSTRAP_SERVERS)
+    .option("topic", OUTPUT_TOPIC)
+    .option("checkpointLocation", "/tmp/checkpoints")
+    .outputMode("append")
+    .start()
+)
+
+query.awaitTermination()
